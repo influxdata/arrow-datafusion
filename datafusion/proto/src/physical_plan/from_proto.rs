e5d618f89ca5300748a164582265f05d4072a097
wiedld <wiedld@users.noreply.github.com>
2024-04-26 03:42:16 -0700

    Allow adding user defined metadata to `ParquetSink` (#10224)
    * chore: make explicit what ParquetWriterOptions are created from a subset of TableParquetOptions
    
    * refactor: restore the ability to add kv metadata into the generated file sink
    
    * test: demomnstrate API contract for metadata TableParquetOptions
    
    * chore: update code docs
    
    * fix: parse on proper delimiter, and improve tests
    
    * fix: enable any character in the metadata string value, by having any key parsing be a part of the format.metadata::key

--- datafusion/proto/src/physical_plan/from_proto.rs
+++ datafusion/proto/src/physical_plan/from_proto.rs
@@ -974,6 +974,7 @@ impl TryFrom<&protobuf::TableParquetOptions> for TableParquetOptions {
                 .unwrap()
                 .unwrap(),
             column_specific_options,
+            key_value_metadata: Default::default(),
         })
     }
 }
